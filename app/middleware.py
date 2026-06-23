1b6e0af6030fd6e9a037efc8a6f8b465e270ef52
lab-kolerr <lab.kolerr@kolerr.com>
2026-06-24 02:19:41 +0700

    Initial commit - app/middleware.py

---
+++ app/middleware.py
@@ -0,0 +1,10 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+import logging
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        logging.info(f"Request: {request.method} {request.url}")
+        response = await call_next(request)
+        logging.info(f"Response: {response.status_code}")
+        return response
